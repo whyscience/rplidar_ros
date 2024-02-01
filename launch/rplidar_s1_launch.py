4d089ad80db37df1aa555937b15fe18cc9ea4cdb
eric <haiyang.wang@clobotics.com>
2024-02-01 21:31:07 +0800

    change for wheeltec laser

--- launch/rplidar_s1_launch.py
+++ launch/rplidar_s1_launch.py
@@ -12,7 +12,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     channel_type = LaunchConfiguration('channel_type', default='serial')
-    serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
+    serial_port = LaunchConfiguration('serial_port', default='/dev/wheeltec_laser')
     serial_baudrate = LaunchConfiguration('serial_baudrate', default='256000') 
     frame_id = LaunchConfiguration('frame_id', default='laser')
     inverted = LaunchConfiguration('inverted', default='false')
